4db70bb93126ca4dad6a6df8fe2aae125ef5321d
Alex <rct.mancevic@bsu.by>
2022-04-16 19:07:54 +0300

    Interface

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,8 @@ package com.company;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.lang.reflect.Field;
 
 public class Main extends JFrame {
@@ -15,14 +17,20 @@ public class Main extends JFrame {
     private static final int OUTGOING_AREA_DEFAULT_ROWS = 5;
 
     private final JTextArea textAreaIncoming;
+    private final JTextArea textAreaOutgoing;
 
-
+    private final JTextField textFieldFrom;
+    private final JTextField textFieldTo;
     private JPanel field = new JPanel();
 
+    private static final int SMALL_GAP = 5;
+    private static final int MEDIUM_GAP = 10;
+    private static final int LARGE_GAP = 15;
 
 
     public Main() {
         super ("Лабараторная работа №7");
+
         setSize(WIDTH, HEIGHT);
         setMinimumSize(
                 new Dimension(WIDTH, HEIGHT));
@@ -30,13 +38,83 @@ public class Main extends JFrame {
 
         setLocation((kit.getScreenSize().width - WIDTH)/2,
                 (kit.getScreenSize().height - HEIGHT)/2);
-        getContentPane().add(field);
+
 
         textAreaIncoming = new JTextArea(INCOMING_AREA_DEFAULT_ROWS, 0);
         textAreaIncoming.setEditable(false);
         final JScrollPane scrollPaneIncoming =
                 new JScrollPane(textAreaIncoming);
-        field.add (textAreaIncoming);
+
+        textAreaOutgoing = new JTextArea(OUTGOING_AREA_DEFAULT_ROWS, 0);
+        textAreaIncoming.setEditable(false);
+        final JScrollPane scrollPaneOutgoing =
+                new JScrollPane(textAreaIncoming);
+
+        final JLabel labelFrom = new JLabel("Подпись");
+        final JLabel labelTo = new JLabel("Получатель");
+        textFieldFrom = new JTextField(FROM_FIELD_DEFAULT_COLUMNS);
+        textFieldTo = new JTextField(TO_FIELD_DEFAULT_COLUMNS);
+
+
+
+        final JPanel messagePanel = new JPanel();
+        messagePanel.setBorder(
+                BorderFactory.createTitledBorder("Сообщение"));
+
+        final JButton sendButton = new JButton("Отправить");
+        sendButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                ;
+            }
+        });
+
+
+        final GroupLayout layout2 = new GroupLayout(messagePanel);
+        messagePanel.setLayout(layout2);
+        layout2.setHorizontalGroup(layout2.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout2.createParallelGroup(GroupLayout.Alignment.TRAILING)
+                        .addGroup(layout2.createSequentialGroup()
+                                .addComponent(labelFrom)
+                                .addGap(SMALL_GAP)
+                                .addComponent(textFieldFrom)
+                                .addGap(LARGE_GAP)
+                                .addComponent(labelTo)
+                                .addGap(SMALL_GAP)
+                                .addComponent(textFieldTo))
+                        .addComponent(scrollPaneOutgoing)
+                        .addComponent(sendButton))
+                .addContainerGap());
+        layout2.setVerticalGroup(layout2.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout2.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                        .addComponent(labelFrom)
+                        .addComponent(textFieldFrom)
+                        .addComponent(labelTo)
+                        .addComponent(textFieldTo))
+                .addGap(MEDIUM_GAP)
+                .addComponent(scrollPaneOutgoing)
+                .addGap(MEDIUM_GAP)
+                .addComponent(sendButton)
+                .addContainerGap());
+
+
+
+        final GroupLayout layout1 = new GroupLayout(getContentPane());
+        setLayout(layout1);
+        layout1.setHorizontalGroup(layout1.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout1.createParallelGroup()
+                        .addComponent(scrollPaneIncoming)
+                        .addComponent(messagePanel))
+                .addContainerGap());
+        layout1.setVerticalGroup(layout1.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(scrollPaneIncoming)
+                .addGap(MEDIUM_GAP)
+                .addComponent(messagePanel)
+                .addContainerGap());
 
     }
 
