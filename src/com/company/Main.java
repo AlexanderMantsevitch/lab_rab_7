e6ee7682861491145d62423df55cd4b1a4830368
Alex <rct.mancevic@bsu.by>
2022-04-16 15:24:45 +0300

    Создание main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,34 @@
 package com.company;
 
-public class Main {
+import javax.swing.*;
+import java.awt.*;
+import java.lang.reflect.Field;
+
+public class Main extends JFrame {
+
+    private static final int WIDTH = 700;
+    private static final int HEIGHT = 500;
+    private JPanel field = new JPanel();
+
+
+
+    public Main() {
+        super ("Лабараторная работа №7");
+        setSize(WIDTH, HEIGHT);
+        Toolkit kit = Toolkit.getDefaultToolkit();
+
+        setLocation((kit.getScreenSize().width - WIDTH)/2,
+                (kit.getScreenSize().height - HEIGHT)/2);
+        getContentPane().add(field);
+
+    }
+
+
 
     public static void main(String[] args) {
-        System.out.println("Hello, world");
+       Main frame = new Main();
+       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       frame.setVisible(true);
+
     }
 }
