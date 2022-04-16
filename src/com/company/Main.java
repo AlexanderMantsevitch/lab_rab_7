8b3c7c3955829f34eecbded26e4fda29e8c302d1
Alex <rct.mancevic@bsu.by>
2022-04-16 18:14:48 +0300

    Поле вывода сообщений

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,8 +6,17 @@ import java.lang.reflect.Field;
 
 public class Main extends JFrame {
 
-    private static final int WIDTH = 700;
+    private static final int WIDTH = 500;
     private static final int HEIGHT = 500;
+
+    private static final int FROM_FIELD_DEFAULT_COLUMNS = 10;
+    private static final int TO_FIELD_DEFAULT_COLUMNS = 20;
+    private static final int INCOMING_AREA_DEFAULT_ROWS = 10;
+    private static final int OUTGOING_AREA_DEFAULT_ROWS = 5;
+
+    private final JTextArea textAreaIncoming;
+
+
     private JPanel field = new JPanel();
 
 
@@ -15,11 +24,19 @@ public class Main extends JFrame {
     public Main() {
         super ("Лабараторная работа №7");
         setSize(WIDTH, HEIGHT);
+        setMinimumSize(
+                new Dimension(WIDTH, HEIGHT));
         Toolkit kit = Toolkit.getDefaultToolkit();
 
         setLocation((kit.getScreenSize().width - WIDTH)/2,
                 (kit.getScreenSize().height - HEIGHT)/2);
         getContentPane().add(field);
+
+        textAreaIncoming = new JTextArea(INCOMING_AREA_DEFAULT_ROWS, 0);
+        textAreaIncoming.setEditable(false);
+        final JScrollPane scrollPaneIncoming =
+                new JScrollPane(textAreaIncoming);
+        field.add (textAreaIncoming);
 
     }
 
